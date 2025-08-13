4dbd11f94f48b6b827133843ed17192ceab4ed15
Ozzy-ZY <zyadhimself1@gmail.com>
2025-08-13 13:38:18 +0300

    add GET product endpoint

--- src/main/java/org/ozzy/product/servlet/ProductServlet.java
+++ src/main/java/org/ozzy/product/servlet/ProductServlet.java
@@ -1,17 +1,31 @@
 package org.ozzy.product.servlet;
 
+import com.google.gson.Gson;
+import org.ozzy.product.data.DataStore;
+import org.ozzy.product.data.DataStoreImpl;
+import org.ozzy.product.data.Product;
 import java.io.*;
+import java.util.Collection;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "product-servlet", value = "/product")
 public class ProductServlet extends HttpServlet {
+    private final DataStore dataStore = DataStoreImpl.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
+        addJsonHeaders(resp);
+        Collection<Product> resultObj = dataStore.getProducts();
+        Gson gson = new Gson();
+        String json = gson.toJson(resultObj.toArray());
         PrintWriter out = resp.getWriter();
-        out.println("{\"name\":\"Product\"}");
+        out.println(json);
+        out.flush();
         out.close();
+    }
+    private static void addJsonHeaders(HttpServletResponse resp) {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
     }
 }
